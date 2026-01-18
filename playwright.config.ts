daaf898d065151c31d111b069f058ba4c4f37e9b
TUCCHI1 <tutikikin1@gmail.com>
2026-01-18 20:11:45 +0900

    fix: Remove ternary operator from playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from '@playwright/test';
 
+function getReporter() {
+  if (process.env.CI) {
+    return 'html';
+  }
+  return 'list';
+}
+
 export default defineConfig({
   testDir: './tests',
   testMatch: 'spotlight.spec.ts',
   timeout: 60000,
-  reporter: process.env.CI ? 'html' : 'list',
+  reporter: getReporter(),
   webServer: {
     command: 'npx serve tests -l 3333 --no-clipboard',
     port: 3333,
