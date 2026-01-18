8ff2d45c77ad24d812559be566a7dadd7ae2f050
TUCCHI1 <tutikikin1@gmail.com>
2026-01-18 20:09:42 +0900

    ci: Follow Playwright official CI best practices

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   testDir: './tests',
   testMatch: 'spotlight.spec.ts',
   timeout: 60000,
+  reporter: process.env.CI ? 'html' : 'list',
   webServer: {
     command: 'npx serve tests -l 3333 --no-clipboard',
     port: 3333,
